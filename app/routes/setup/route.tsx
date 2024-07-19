e06ac2e228c47edc7eb4d0784c5a9e80b6dbafc2
Thom C <thomas.crater@epita.fr>
2024-07-19 23:57:18 +0200

    Import fix

--- app/routes/setup/route.tsx
+++ app/routes/setup/route.tsx
@@ -1,7 +1,7 @@
 import { ActionFunctionArgs, LoaderFunctionArgs } from "@remix-run/node";
 import { useActionData, Form, Link, json, redirect, useLoaderData } from "@remix-run/react";
 import { getSession, commitSession, requireId, requireVerified, csrf_validation, destroySession, csrf_token } from "../../sessions.server";
-import { validate_password } from "../signup/validate.server";
+import { validate_password } from "./validate.server";
 import { setupAccount } from "./queries.server";
 
 interface ValidationErrors
